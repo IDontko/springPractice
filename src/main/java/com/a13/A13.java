c635dd8eb045febcea52b407d8e1bac8cc399b67
gaoyang <252128492@qq.com>
2022-06-28 23:50:41 +0800

    method.invoke(p, args)不能调用会引起死循环

--- src/main/java/com/a13/A13.java
+++ src/main/java/com/a13/A13.java
@@ -14,11 +14,13 @@ public class A13 {
             @Override
             public Object intercept(Object p, Method method, Object[] args,
                                     MethodProxy methodProxy) throws Throwable {
-//                System.out.println("before...");
+                System.out.println("before...");
 //                return method.invoke(target, args); // 反射调用
                 // FastClass
 //                return methodProxy.invoke(target, args); // 内部无反射, 结合目标用
                 return methodProxy.invokeSuper(p, args); // 内部无反射, 结合代理用
+                //为啥不能这么写? 因为如果调用代理类的 实现接口，该接口会继续进入intercept方法，导致死循环。
+//                return methodProxy.invoke(p, args);
             }
         });
 
